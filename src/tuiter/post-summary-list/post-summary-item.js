5413c4072a33856cf619c10b922e39911fc8a49a
A3M4 <419714952@qq.com>
2022-10-23 17:35:14 -0400

    Fixed error in Netlify

--- src/tuiter/post-summary-list/post-summary-item.js
+++ src/tuiter/post-summary-list/post-summary-item.js
@@ -9,7 +9,7 @@ const PostSummaryItem = (
     return (
         <li className="list-group-item">
             <div className="row">
-                <div className="col-10">
+                <div className="col-10" >
                     <div>{post.userName} . {post.time}</div>
                     <div className="fw-bolder">{post.topic}</div>
                     <div>{post.title}</div>
