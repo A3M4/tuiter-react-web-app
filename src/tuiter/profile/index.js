6188f81413216b33d312124a67a98812fd5d1eca
A3M4 <419714952@qq.com>
2022-11-20 22:54:19 -0500

    a8 REST fa22

--- src/tuiter/profile/index.js
+++ src/tuiter/profile/index.js
@@ -5,8 +5,6 @@ import {Link} from "react-router-dom";
 
 const Profile = () => {
     const profile = useSelector(state => state.profile);
-
-
     return (
         <>
             <Link to="/tuiter">
--- src/tuiter/tuits/tuit-list.js
+++ src/tuiter/tuits/tuit-list.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useEffect} from "react";
+import {useDispatch, useSelector}
+    from "react-redux";
 import TuitItem from "./tuit-item";
-import {useSelector} from "react-redux";
+import {findTuitsThunk}
+    from "../../services/tuits-thunks";
 
-const TuitList = () => {
-    const tuitsArray = useSelector(state => state.tuits);
+const TuitsList = () => {
+    const {tuits, loading} = useSelector(
+        state => state.tuitsData)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findTuitsThunk())
+    }, [])
     return (
         <ul className="list-group">
             {
-                tuitsArray.map(post =>
-                    <TuitItem key={post._id} post={post}/>)
+                loading &&
+                <li className="list-group-item">
+                    Loading...
+                </li>
             }
+            ...
         </ul>
-    )
-}
-
-export default TuitList;
+    );
+};
+export default TuitsList;
